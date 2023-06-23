4f3d57c675b700d4497d29c10d4c3ca13e5baed7
Margot Salsmann <margot.salsmann@gmail.com>
2023-06-23 13:32:44 +0200

    correction

--- app.js
+++ app.js
@@ -10,10 +10,83 @@ const BMIData = [
 // IMC = poids en kg / taille² en m
 
 
+/******** Valeurs et calcul ********
+ * Récupérer les valeurs des inputs
+ * Regex : uniquement des chiffres
+ * Calculer IMC = poids en kg / taille² en m
+ */
 
-/******** récupérer les valeurs des inputs ****/
+// le formulaire est un objet du DOM (html) >> typeof form = object
+// Pour l'affichage de toutes les propriétés de l'objet : console.dir(form)
+const form = document.querySelector("form");
 
-let btnCalculateIMC = document.querySelector("button")
+// évenement avec 2 arguments : nom de l'événement (submit) et meth call back (fonction appelée par une autre fonction)
+// addEventListener est une méthode = fonction qui est sous forme de propriété dans un objet
+form.addEventListener("submit", handleForm);
+
+function handleForm(e) {
+  e.preventDefault();
+
+  calculateBMI();
+}
+
+// objet node (sans [..]) ou tableau (avec [...document.querySelector]) permettant présentation de toutes les méthodes)
+const inputs = document.querySelectorAll("input");
+
+// 1er élément = index 0 et 2e élément = index 1
+function calculateBMI() {
+  const height = inputs[0].value;
+  const weight = inputs[1].value;
+
+  // truthy
+  // falsy = "" null undefined false
+  
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    handleError();
+    return;
+  }
+
+  // calcul IMC = meth math.pow (mettre au carré)
+  // height / 100 pr obtenir taille en m et plus en cm
+  // meth toFixed(1) = s'arrêter à 1 chiffre décimal
+  const BMI = (weight / Math.pow(height / 100, 2)).toFixed(1);
+  console.log(BMI);
+
+  showResult(BMI);
+}
+
+const displayBMI = document.querySelector(".bmi-value");
+const result = document.querySelector(".result");
+
+function handleError() {
+  displayBMI.textContent = "Oups!";
+  displayBMI.style.color = "inherit";
+  result.textContent = "Remplissez correctement votre taille et votre poids";
+}
+
+/****** Fonction montrer le résultat ******
+ * remplir les deux conditions pour obtenir données
+ * prendre en compte le dernier cas où il n'y a pas de tableau 
+ */
+
+function showResult(BMI) {
+  const rank = BMIData.find(data => {
+    if (BMI >= data.range[0] && BMI < data.range[1]) return data;
+    else if (typeof data.range === "number" && BMI >= data.range) return data;
+  });
+
+  console.log(rank)
+
+  displayBMI.textContent = BMI;
+  displayBMI.style.color = `${rank.color}`;
+  result.textContent = `Résultat : ${rank.name}`;
+}
+
+
+
+/****** MA VERSION ******
+
+const btnCalculateIMC = document.querySelector("button")
 
 btnCalculateIMC.addEventListener("click", (event) => {
   event.preventDefault();
@@ -29,20 +102,19 @@ btnCalculateIMC.addEventListener("click", (event) => {
       console.log("InputRegex ok")
       //meth Math.sqrt() : renvoyer racine carrée d'un nombre
       let IMC = height * Math.sqrt(weight)
-      console.log(IMC)    
+      console.log(IMC)
+      toRankImg()  
   } else {
         console.log(("InputRegex non ok"))
         alert("Veuillez renseigner votre taille en cm et votre poids en kg");
   }
 
-
-  
-  /*if (!height || !weight) {
+  if (!height || !weight) {
     alert("Veuillez renseigner votre taille et votre poids");
   } else {
     //meth Math.sqrt() : renvoyer racine carrée d'un nombre
     let IMC = height * Math.sqrt(weight)
     console.log(IMC)
-  }*/
-})
-
+  }
+  
+  */
